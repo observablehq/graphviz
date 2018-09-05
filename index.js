5fad928ae013f8df797604693bd1fddfcbdbff7b
Mike Bostock <mbostock@gmail.com>
2018-09-05 08:35:29 -0700

    Simplify.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import Viz from "./viz.js";
 import {render, Module} from "./viz-lite.render.js";
 
+const defaults = {files: [], format: "svg", engine: "dot"};
+
 function Dot(options) {
-  const viz = new Viz({render, Module: () => Module(options)});
+  const module = Module(options = Object.assign({}, defaults, options));
   return async function dot(strings) {
     let string = strings[0] + "", i = 0, n = arguments.length;
     while (++i < n) string += arguments[i] + "" + strings[i];
-    const svg = document.importNode(await viz.renderSVGElement(string, options), true);
+    const template = document.createElement("template");
+    template.innerHTML = await render(module, string, options);
+    const svg = document.importNode(template.content.firstElementChild, true);
     svg.style.maxWidth = "100%";
     svg.style.height = "auto";
     return svg;
