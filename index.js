5c0638688e19a6b2c4b2f055e8250f0277d6a3e8
Mike Bostock <mbostock@gmail.com>
2018-09-29 15:26:06 -0700

    Recover from error. Fixes #3.

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import {render, Module} from "./viz-lite.render.js";
 const defaults = {files: [], format: "svg", engine: "dot"};
 
 function Dot(options) {
-  const module = Module(options = Object.assign({}, defaults, options));
+  options = Object.assign({}, defaults, options);
+  let module = Module(Object.assign({}, options));
   return async function dot(strings) {
     let string = strings[0] + "", i = 0, n = arguments.length;
     while (++i < n) string += arguments[i] + "" + strings[i];
     const template = document.createElement("template");
-    template.innerHTML = await render(module, string, options);
+    try {
+      template.innerHTML = render(module, string, options);
+    } catch (error) {
+      module = Module(Object.assign({}, options)); // See Viz.js caveats.
+      throw error;
+    }
     const svg = document.importNode(template.content.firstElementChild, true);
     svg.style.maxWidth = "100%";
     svg.style.height = "auto";
